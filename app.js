6b97fffb58e42f463b33bbf18219e186f5642154
Dave Killough <dave.killough@gmail.com>
2019-09-19 12:06:31 -0400

    latest

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var controllers = require('./controllers');
+var routes = require('./routes');
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', controllers);
+app.use('/', routes);
 app.use(function(req, res, next) {
   next(createError(404));
 });
